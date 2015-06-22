0c71ad5a0d98e1a997666221d503e373679976f9
Eugenity <Eugenity@MacBook-Pro-MacBook.local>
2015-06-22 17:57:52 +0300

    Initial Commit

---
+++ BizrateRewards/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BizrateRewards
+//
+//  Created by Eugenity on 22.06.15.
+//  Copyright (c) 2015 ThinkMobiles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BizrateRewards/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BizrateRewards
+//
+//  Created by Eugenity on 22.06.15.
+//  Copyright (c) 2015 ThinkMobiles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
