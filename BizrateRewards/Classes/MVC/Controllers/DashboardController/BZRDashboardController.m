64a15ed4c60fffe6b069ebb3d7b20c5f3f76f91c
Eugenity <Eugenity@MacBook-Pro-MacBook.local>
2015-06-23 18:07:15 +0300

    dashboard controller has been added

---
+++ BizrateRewards/Classes/MVC/Controllers/DashboardController/BZRDashboardController.m
@@ -0,0 +1,24 @@
+//
+//  BZRDashboardController.m
+//  BizrateRewards
+//
+//  Created by Eugenity on 23.06.15.
+//  Copyright (c) 2015 ThinkMobiles. All rights reserved.
+//
+
+#import "BZRDashboardController.h"
+
+@interface BZRDashboardController ()
+
+@end
+
+@implementation BZRDashboardController
+
+#pragma mark - View Lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+@end
