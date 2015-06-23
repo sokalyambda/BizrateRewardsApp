be4fa1b4b3bc6989d238b3c632691f760c8544b1
Eugenity <Eugenity@MacBook-Pro-MacBook.local>
2015-06-23 23:20:53 +0300

    ask permissions for push notifications

--- BizrateRewards/AppDelegate.m
+++ BizrateRewards/AppDelegate.m
@@ -20,7 +20,53 @@
     pageControl.pageIndicatorTintColor = [UIColor lightGrayColor];
     pageControl.currentPageIndicatorTintColor = [UIColor blackColor];
     pageControl.backgroundColor = [UIColor whiteColor];
+    
+    [self registerApplicationForPushNotifications:application];
+    
     return YES;
+}
+
+- (void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings
+{
+    [application registerForRemoteNotifications];
+}
+
+- (void)application:(UIApplication *)application handleActionWithIdentifier:(NSString *)identifier forRemoteNotification:(NSDictionary *)userInfo completionHandler:(void(^)())completionHandler
+{
+    
+}
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+    NSString *token = [[[[deviceToken description]
+                                              stringByReplacingOccurrencesOfString:@"<" withString:@""]
+                                             stringByReplacingOccurrencesOfString:@">" withString:@""]
+                                            stringByReplacingOccurrencesOfString:@" " withString:@""];
+    NSLog(@"%@", deviceToken);
+}
+
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
+{
+    NSLog(@"%@", error);
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+{
+    NSLog(@"PUSH %@", userInfo);
+}
+
+- (void)registerApplicationForPushNotifications:(UIApplication *)application
+{
+    if ([[UIDevice currentDevice].systemVersion floatValue] > 8.0) {
+        UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:(UIRemoteNotificationTypeBadge
+                                                                                             |UIRemoteNotificationTypeSound
+                                                                                             |UIRemoteNotificationTypeAlert) categories:nil];
+        [application registerUserNotificationSettings:settings];
+        [application registerForRemoteNotifications];
+    } else {
+        UIRemoteNotificationType myTypes = UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeSound;
+        [application registerForRemoteNotificationTypes:myTypes];
+    }
 }
 
 @end
