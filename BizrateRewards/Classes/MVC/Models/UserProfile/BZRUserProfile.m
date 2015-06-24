6710cbc07ea8d4c8e85ad1b50fc00a07e107bf1e
Eugenity <Eugenity@MacBook-Pro-MacBook.local>
2015-06-24 15:24:05 +0300

    UserProfile changes

---
+++ BizrateRewards/Classes/MVC/Models/UserProfile/BZRUserProfile.m
@@ -0,0 +1,23 @@
+//
+//  BZRUserProfile.m
+//  BizrateRewards
+//
+//  Created by Eugenity on 24.06.15.
+//  Copyright (c) 2015 ThinkMobiles. All rights reserved.
+//
+
+#import "BZRUserProfile.h"
+
+@implementation BZRUserProfile
+
+- (instancetype)initWithServerResponse:(NSDictionary *)response
+{
+    self = [super init];
+    if (self) {
+        
+    }
+    return self;
+}
+
+
+@end
