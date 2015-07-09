bdd380a33b28d4802d41a8d586d518649d18942b
polovka <myroslava.polovka@gmail.com>
2015-07-09 16:53:09 +0300

    several changes

---
+++ BizrateRewards/Classes/Cells/GiftcardCell/BZRCiftcardCell.h
@@ -0,0 +1,13 @@
+//
+//  BZRCiftcardCell.h
+//  BizrateRewards
+//
+//  Created by Admin on 09.07.15.
+//  Copyright (c) 2015 ThinkMobiles. All rights reserved.
+//
+
+@interface BZRCiftcardCell : UICollectionViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *giftcardImageView;
+
+@end
---
+++ BizrateRewards/Classes/Cells/GiftcardCell/BZRCiftcardCell.m
@@ -0,0 +1,18 @@
+//
+//  BZRCiftcardCell.m
+//  BizrateRewards
+//
+//  Created by Admin on 09.07.15.
+//  Copyright (c) 2015 ThinkMobiles. All rights reserved.
+//
+
+#import "BZRCiftcardCell.h"
+
+@interface BZRCiftcardCell ()
+
+
+@end
+
+@implementation BZRCiftcardCell
+
+@end
