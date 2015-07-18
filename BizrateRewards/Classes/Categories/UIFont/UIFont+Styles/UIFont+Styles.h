c88505daf1c408cddd9fb5cd13bc552715cb7ffb
Eugenity <Eugenity@MacBook-Pro-MacBook.local>
2015-07-18 19:22:04 +0300

    status bar color changed

---
+++ BizrateRewards/Classes/Categories/UIFont/UIFont+Styles/UIFont+Styles.h
@@ -0,0 +1,13 @@
+//
+//  UIFont+Styles.h
+//  BizrateRewards
+//
+//  Created by Eugenity on 18.07.15.
+//  Copyright (c) 2015 Connexity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIFont (Styles)
+
+@end
