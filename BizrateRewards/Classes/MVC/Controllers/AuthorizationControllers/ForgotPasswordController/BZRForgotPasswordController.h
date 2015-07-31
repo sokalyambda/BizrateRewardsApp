20d13cce785b2bad9c1a1223856f6be7316b247c
Eugenity <Eugenity@MacBook-Pro-MacBook.local>
2015-07-31 16:25:47 +0300

    Forgot password controller has been added

---
+++ BizrateRewards/Classes/MVC/Controllers/AuthorizationControllers/ForgotPasswordController/BZRForgotPasswordController.h
@@ -0,0 +1,13 @@
+//
+//  BZRForgotPasswordController.h
+//  BizrateRewards
+//
+//  Created by Eugenity on 31.07.15.
+//  Copyright (c) 2015 Connexity. All rights reserved.
+//
+
+#import "BZRBaseAuthController.h"
+
+@interface BZRForgotPasswordController : BZRBaseAuthController
+
+@end
---
+++ BizrateRewards/Classes/MVC/Controllers/AuthorizationControllers/ForgotPasswordController/BZRForgotPasswordController.m
@@ -0,0 +1,24 @@
+//
+//  BZRForgotPasswordController.m
+//  BizrateRewards
+//
+//  Created by Eugenity on 31.07.15.
+//  Copyright (c) 2015 Connexity. All rights reserved.
+//
+
+#import "BZRForgotPasswordController.h"
+
+@interface BZRForgotPasswordController ()
+
+@end
+
+@implementation BZRForgotPasswordController
+
+#pragma mark - Lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+@end
